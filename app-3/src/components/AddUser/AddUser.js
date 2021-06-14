c0f01bbca6efe97b2900856609d384ef602bcb18
Jayraj Shah <jayrajcshah@gmail.com>
2021-06-14 16:22:34 +0530

    Added Refs | Section 9

--- app-3/src/components/AddUser/AddUser.js
+++ app-3/src/components/AddUser/AddUser.js
@@ -1,38 +1,26 @@
-import React, { useState } from "react";
+import React, { useState, useRef } from "react";
 import Button from "../UI/Button";
 import Card from "../UI/Card";
 import styles from "./AddUser.module.css";
 
-const INITIAL_STATE = {
-  name: "",
-  age: "",
-};
 const AddUser = ({ onAddUserHandler }) => {
-  const [inputData, setInputData] = useState(INITIAL_STATE);
-
-  const onChangeHandler = (event) => {
-    setInputData((prevData) => ({
-      ...prevData,
-      [event.target.id]: event.target.value,
-    }));
-  };
+  const nameInput = useRef();
+  const ageInput = useRef();
 
   const onSubmitHandler = (event) => {
     event.preventDefault();
+    const name = nameInput.current.value;
+    const age = ageInput.current.value;
 
-    if (
-      inputData.name.length === 0 ||
-      inputData.age.length === 0 ||
-      +inputData.age < 0
-    )
-      return;
+    if (name.length === 0 || age.length === 0 || +age < 0) return;
 
     const data = {
-      name: inputData.name,
-      age: +inputData.age,
+      name,
+      age,
     };
     onAddUserHandler(data);
-    setInputData(INITIAL_STATE);
+    nameInput.current.value = "";
+    ageInput.current.value = "";
   };
 
   return (
@@ -40,21 +28,11 @@ const AddUser = ({ onAddUserHandler }) => {
       <form className={styles.form__control}>
         <div className={styles.form__inputs}>
           <label htmlFor="name">Name</label>
-          <input
-            type="text"
-            id="name"
-            value={inputData.name}
-            onChange={onChangeHandler}
-          />
+          <input type="text" id="name" ref={nameInput} />
         </div>
         <div className={styles.form__inputs}>
           <label htmlFor="age">Age</label>
-          <input
-            type="number"
-            id="age"
-            value={inputData.age}
-            onChange={onChangeHandler}
-          />
+          <input type="number" id="age" ref={ageInput} />
         </div>
         <Button title="Add" type="submit" onClick={onSubmitHandler} />
       </form>
